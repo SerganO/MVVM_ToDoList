04e42c2a0bb3bbda7d4ecea2890de222e8db6c50
Serhii Ostrovetskyi <so@rollncode.com>
2019-06-11 16:56:33 +0300

    init

---
+++ MVVM_ToDoList/MVVM_ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_ToDoList
+//
+//  Created by Trainee on 6/11/19.
+//  Copyright © 2019 Trainee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
